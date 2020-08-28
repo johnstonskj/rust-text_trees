4bc446c079e465f3f7a35fe89b2e76292cf9a32b
Simon Johnston <johnstonskj@gmail.com>
2020-08-28 15:05:40 -0700

    Added warning config to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -79,6 +79,26 @@ root
 
 */
 
+#![warn(
+    // ---------- Stylistic
+    future_incompatible,
+    nonstandard_style,
+    rust_2018_idioms,
+    trivial_casts,
+    trivial_numeric_casts,
+    // ---------- Public
+    missing_debug_implementations,
+    missing_docs,
+    unreachable_pub,
+    // ---------- Unsafe
+    unsafe_code,
+    // ---------- Unused
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    unused_results,
+)]
+
 use std::fmt::Display;
 use std::io::Result;
 use std::io::Write;
